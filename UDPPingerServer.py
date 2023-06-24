42855e61139f3b69f200bdc4184ca8d31f5c4337
Navindren Letchumanan <136433170+VglntzrX-MKII@users.noreply.github.com>
2023-06-24 15:33:00 +0800

    Implemented WAN Connectivity and Auto Command Creation of UDPPingerClient
    - This Python script will now check for Internet Connectivity and if found, will serve the system's public IPV4 IP in addition to its LAN IPV4 IP. (note that WAN option will only work for servers that are not behind a CG-NAT connection)
    
    - Implemented Auto Command Creation with suitable parameters to launch the client application. (why type it every single time when you can automate it?)

--- UDPPingerServer.py
+++ UDPPingerServer.py
@@ -1,12 +1,46 @@
 import random
 import sys
+import urllib.request
+import http.client as httplib
 from socket import *
 
 # Check command line arguments
 if len(sys.argv) != 2:
-    print ("Launch Parameter : python UDPPingerServer PORT_NUM")
-    print ("Example : python UDPPingerServer 6969") 
+    print ("Launch Parameter : python UDPPingerServer.py PORT_NUM")
+    print ("Example : python UDPPingerServer.py 6969") 
     sys.exit()
+
+# Check for Internet Connection
+
+def internet() -> bool:
+    conn = httplib.HTTPSConnection("8.8.8.8", timeout=5)
+    try:
+        conn.request("HEAD", "/")
+        return True
+    except Exception:
+        return False
+    finally:
+        conn.close()
+
+# Retrieve host system's IP and Hostname (for creating launch command for the client program)
+hostname=gethostname()   
+IPAddr=gethostbyname(hostname)
+if internet():
+    PubIPAddr= urllib.request.urlopen('https://ipv4.seeip.org').read().decode('utf8')
+
+print("Server's Hostname: "+hostname)   
+print("Server's Private IP Address: "+IPAddr)
+if internet():
+    print("Server's Public IP Address: " +PubIPAddr)
+print("Server's Port: "+sys.argv[1])
+print("Use these as launch parameters to launch the client program.")
+print("You can copy the following command(s) and run in a seperate Command Shell / Terminal Session")
+print ("LAN Access: " + "python UDPPingerClient.py " + IPAddr + " " + sys.argv[1])
+
+if internet():
+    print ("WAN Access (limited): " + "python UDPPingerClient.py " + PubIPAddr + " " + sys.argv[1])
+else:
+    print ("Internet Access Unavailable.")
 
 # Create a UDP socket
 # Notice the use of SOCK_DGRAM for UDP packets
@@ -32,5 +66,4 @@ while True:
     #Navindren Debug Params
     # Decode UTF-8 message received from Client Side
     message = message.decode()
-
     print (message)
